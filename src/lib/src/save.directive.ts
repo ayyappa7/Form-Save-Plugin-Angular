9f88b1db83c92dfa13407155ea5e7e6707e5d0fc
ayyappa <lalit@customerxps.com>
2018-04-06 16:08:13 +0530

    updated save emitter

--- src/lib/src/save.directive.ts
+++ src/lib/src/save.directive.ts
@@ -1,59 +1,76 @@
-import {Directive, ElementRef, Input, QueryList, ViewChildren, OnInit, EventEmitter, Output} from '@angular/core';
+import {
+    Directive,
+    ElementRef,
+    Input,
+    QueryList,
+    ViewChildren,
+    OnInit,
+    OnDestroy,
+    EventEmitter,
+    Output
+} from '@angular/core';
 
 @Directive({
     selector: '[saveField]'
 })
-export class SaveDirective implements OnInit{
+export class SaveDirective implements OnInit, OnDestroy {
     @Input('saveField') saveField: string;
     @Input('interval') interval: number;
-    @Input('userKey') userKey: string ;
-    @Output() onSave = new EventEmitter< string>()
-    constructor(private el:ElementRef) {
+    @Input('userKey') userKey: string;
+    @Output() onSave = new EventEmitter<string>();
+    intervalId: number;
+
+    constructor(private el: ElementRef) {
     }
+
     ngOnInit() {
         if (this.interval > 0) {
-            setInterval(() => {
+            this.intervalId = setInterval(() => {
                 this.save();
             }, this.interval * 1000);
-        }else{
-            if(localStorage.getItem(this.userKey)!=null){
-                let map=JSON.parse(localStorage.getItem(this.userKey))
-                map[this.saveField]=btoa("")
-                localStorage.setItem(this.userKey,JSON.stringify(map))
+        } else {
+            if (localStorage.getItem(this.userKey) != null) {
+                let map = JSON.parse(localStorage.getItem(this.userKey))
+                map[this.saveField] = btoa("")
+                localStorage.setItem(this.userKey, JSON.stringify(map))
             }
         }
     }
 
-    save(){
-        this.onSave.emit("Saved");
-        if(this.el.nativeElement.value!=""){
-            let map={}
-            if(localStorage.getItem(this.userKey)==null){
-                map[this.saveField]=btoa(this.el.nativeElement.value);
-                // console.log(map)
-                localStorage.setItem(this.userKey,JSON.stringify(map))
-            }else{
-                map=JSON.parse(localStorage.getItem(this.userKey))
-                map[this.saveField]=btoa(this.el.nativeElement.value);
-                localStorage.setItem(this.userKey,JSON.stringify(map))
-                // console.log(map)
+
+    save() {
+        let currValue = this.el.nativeElement.value;
+        if (currValue != "") {
+            let map = {};
+            if (localStorage.getItem(this.userKey) == null) {
+                map[this.saveField] = btoa(currValue);
+                localStorage.setItem(this.userKey, JSON.stringify(map))
+            } else {
+                map = JSON.parse(localStorage.getItem(this.userKey));
+                let mapValue = atob(map[this.saveField]);
+                if (currValue != mapValue) {
+                    map[this.saveField] = btoa(currValue);
+                    localStorage.setItem(this.userKey, JSON.stringify(map))
+                    this.onSave.emit("Saved ")
+                }
             }
         }
-        // localStorage.setItem(this.saveField,this.el.nativeElement.value)
     }
 
 
-    restore( ){
-        this.onSave.emit("Restored");
-        if(JSON.parse(localStorage.getItem(this.userKey))!=null) {
+    restore() {
+
+        if (JSON.parse(localStorage.getItem(this.userKey)) != null) {
             let map = JSON.parse(localStorage.getItem(this.userKey))
-            if(map[this.saveField]!=null )
-            {
+            if (map[this.saveField] != null) {
                 let val = atob(map[this.saveField]);
                 this.el.nativeElement.value = val;
+                this.onSave.emit("Restored ");
             }
         }
-            // this.el.nativeElement.value=localStorage.getItem(this.saveField);
     }
 
+    ngOnDestroy(): void {
+        clearInterval(this.intervalId);
+    }
 }
